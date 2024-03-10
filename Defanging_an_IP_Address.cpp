7202215e103b3da488c5f05f9a3118846da5bf49
KhushiSonkusare <you@example.com>
2024-03-11 04:11:06 +0530

    Add Defanging an IP Address

---
+++ Defanging_an_IP_Address.cpp
@@ -0,0 +1,16 @@
+#include <string> 
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        for(int i = 0; i < address.size(); i++) {
+            if(address[i] == '.') {
+                address.insert(i, "[");
+                address.insert(i+2, "]");
+                i += 2;
+            }
+        }
+        return address;
+    }
+};
