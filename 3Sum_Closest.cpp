cc96c7c2d076c37f6351af63fae27507c28de409
KhushiSonkusare <you@example.com>
2024-04-09 12:23:00 +0530

    Add 3Sum Closest

--- 3Sum_Closest.cpp
+++ 3Sum_Closest.cpp
@@ -1,35 +1,38 @@
-#include <vector>
-#include <algorithm> // For sort
-#include <cmath>     // For abs
-
-using std::vector;
-
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         std::sort(nums.begin(), nums.end());
         int n = nums.size();
-        int closestSum = nums[0] + nums[1] + nums[2]; 
-        int minDiff = std::abs(target - closestSum); 
-
+        int fsum = 0, diff = 10000;
         for (int i = 0; i < n - 2; ++i) {
             int left = i + 1;
             int right = n - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                int currentDiff = std::abs(target - sum); 
-                if (currentDiff < minDiff) {
-                    minDiff = currentDiff;
-                    closestSum = sum;
-                }
-                if (sum < target) {
+                int dif = 0;
+                if(target == sum){
+                    return sum;
+                }else{
+                    if(target<0){
+                    dif = target + sum; 
+                }else
+                    dif = (target - sum);
+    
+                if (dif<=diff) {
+                    diff = dif;
+                    fsum =sum;
+                    ++left;
+                    --right;
+                } else if (sum < 0) {
                     ++left;
                 } else {
                     --right;
                 }
+                }
+                
             }
         }
         
-        return closestSum;
+        return fsum;
     }
 };
