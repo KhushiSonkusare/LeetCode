6b13d4881d7ce46e3598cd1f89c3d961e45a63a2
KhushiSonkusare <you@example.com>
2024-04-09 12:29:35 +0530

    Add Search in Rotated Sorted Array

--- Search_in_Rotated_Sorted_Array.cpp
+++ Search_in_Rotated_Sorted_Array.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        if (n < 0)   
-            return -1;
-        if (n == 1)
-            return nums[0] == target ? 0 : -1;
-        int l = 0;
-        int r = n;
-        while (l < r) {
-            int m = (l + r) / 2;
-            int v = nums[m];
-            if (target == v)
-                return m;
-            if (v >= nums[l]) {
-                if (nums[l] <= target && target <= v)
-                    r = m;
-                else
-                    l = m + 1;
-            } else {
-                if (v <= target && target <= nums[r - 1])
-                    l = m + 1;
-                else 
-                    r = m;
-            }
+        auto it = std::find(nums.begin(), nums.end(), target);
+        if(it!=nums.end()){
+            int index = std::distance(nums.begin(), it);
+            return index;
         }
         return -1;
     }
