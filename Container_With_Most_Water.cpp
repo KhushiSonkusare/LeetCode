58b1e01cf09cc12c4c828e989b8543526729cb93
KhushiSonkusare <you@example.com>
2024-04-09 07:18:36 +0530

    Add Container With Most Water

--- Container_With_Most_Water.cpp
+++ Container_With_Most_Water.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int max= 0;
-        int i = 0, j = height.size()-1;
-        while(i<height.size() && j>=0){
-            if((j-i)*min(height[i],height[j])>=max){
-                max =( j-i)*min(height[i],height[j]);
-            }
-            if(height[i]<height[j]){
-                i++;
-            }else{
-                j--;
+        for(int i = 0; i<height.size(); i++){
+            for(int j = i+1; j<height.size(); j++){
+                if((j-i)*min(height[i],height[j])>=max){
+                    max = (j-i)*min(height[i],height[j]);
+                }
             }
         }
         return max;
