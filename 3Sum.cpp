d658d0119a2595ad7ec6b4c206a79bed13ed33dd
KhushiSonkusare <you@example.com>
2024-04-09 07:35:29 +0530

    Add 3Sum

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include <set>
-#include <algorithm> 
+#include <algorithm> // For sort
 
 using std::vector;
 
@@ -8,24 +8,29 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         std::set<vector<int>> uniqueTriplets; 
+        
+    
         std::sort(nums.begin(), nums.end());
-        int n = nums.size();
-        for (int i = 0; i < n - 2; ++i) {
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    uniqueTriplets.insert({nums[i], nums[left], nums[right]});
-                    ++left;
-                    --right;
-                } else if (sum < 0) {
-                    ++left;
-                } else {
-                    --right;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+       
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            for (int j = i + 1; j < nums.size(); ++j) {
+         
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                
+                for (int k = j + 1; k < nums.size(); ++k) {
+                
+                    if (k > j + 1 && nums[k] == nums[k - 1]) continue;
+                    
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        uniqueTriplets.insert({nums[i], nums[j], nums[k]});
+                    }
                 }
             }
         }
+       
         vector<vector<int>> result(uniqueTriplets.begin(), uniqueTriplets.end());
         return result;
     }
