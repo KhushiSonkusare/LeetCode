24b6aa96fb2a64d769dc6d2af5e7dbbd41d9414f
KhushiSonkusare <you@example.com>
2024-04-09 12:08:08 +0530

    Add 3Sum

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,28 +1,32 @@
+#include <vector>
+#include <set>
+#include <algorithm> 
+
+using std::vector;
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> result;
-        int i = 0;
+        std::set<vector<int>> uniqueTriplets; 
+        std::sort(nums.begin(), nums.end());
         int n = nums.size();
-        while(i < n-2) {
-            if(nums[i] > 0) { break; }
-            int left = i+1;
-            int right = n-1;
-            int target = -nums[i];
-            while(left < right) {
-                int sum = nums[left] + nums[right];
-                if(sum == target) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    while(left+1 < n && nums[left] == nums[left+1]){ left++; }
-                    left++;
-                } 
-                while(left < right && nums[left] + nums[right] < target) { left++; }
-                while(left < right && nums[left] + nums[right] > target) { right--; }
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    uniqueTriplets.insert({nums[i], nums[left], nums[right]});
+                    ++left;
+                    --right;
+                } else if (sum < 0) {
+                    ++left;
+                } else {
+                    --right;
+                }
             }
-            while(i+1 < n && nums[i] == nums[i+1]) { i++; }
-            i++;
         }
+        vector<vector<int>> result(uniqueTriplets.begin(), uniqueTriplets.end());
         return result;
     }
 };
