aa30a322bd4df804a0510c41f8a88304e7d39d85
KhushiSonkusare <you@example.com>
2024-04-09 13:25:48 +0530

    Add Time Needed to Buy Tickets

--- Time_Needed_to_Buy_Tickets.cpp
+++ Time_Needed_to_Buy_Tickets.cpp
@@ -1,21 +1,33 @@
+#include <vector>
+#include <climits> // For INT_MAX
+
+using namespace std;
+
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int n = tickets.size();
         int time = 0;
-
-        while (tickets[k] > 0) {
-            for (int i = 0; i < n && tickets[k] > 0; ++i) {
+        
+        // Continue until tickets for type k are exhausted
+        while (tickets[k] != 0) {
+            int minCount = INT_MAX; // Initialize minCount to maximum possible value
+            int totalCount = 0; // Initialize totalCount of non-zero tickets
+            // Find the minimum count of tickets among all types and count non-zero tickets
+            for (int i = 0; i < tickets.size(); ++i) {
                 if (tickets[i] > 0) {
-                    --tickets[i];
-                    ++time;
-                    if (i == k && tickets[k] == 0) {
-                        return time;
+                    ++totalCount; // Increment total count of non-zero tickets
+                    if (tickets[i] < minCount) {
+                        minCount = tickets[i];
                     }
                 }
             }
+            time += minCount * totalCount; // Update time
+            // Reduce the count of tickets for all types by minCount
+            for (int i = 0; i < tickets.size(); ++i) {
+                tickets[i] -= minCount;
+            }
         }
-
+        
         return time;
     }
 };
