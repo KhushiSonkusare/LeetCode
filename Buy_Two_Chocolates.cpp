2e20c7bb2958fb5aaf6dc1746b036ef0798563be
KhushiSonkusare <you@example.com>
2024-05-16 05:40:11 +0530

    Add Buy Two Chocolates

--- Buy_Two_Chocolates.cpp
+++ Buy_Two_Chocolates.cpp
@@ -4,11 +4,11 @@ public:
         sort(prices.begin(), prices.end());
 
         if(size(prices)>1){
-            if(prices[0] + prices[1] <= money){
+            if(prices[0] + prices[1] <= 3){
                 return (3 - prices[0] - prices[1]);
             }
         }
-        return money;
+        return 3;
         
     }
 };
