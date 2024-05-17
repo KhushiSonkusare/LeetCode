92902c0b0dad207ed158af876c579eea7777c007
KhushiSonkusare <you@example.com>
2024-05-17 06:44:28 +0530

    Add Roman to Integer

---
+++ Roman_to_Integer.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        std::unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int count = 0;
+        int n = s.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (i < n - 1 && roman[s[i]] < roman[s[i + 1]]) {
+                count -= roman[s[i]];
+            } else {
+                count += roman[s[i]];
+            }
+        }
+
+        return count;
+    }
+};
