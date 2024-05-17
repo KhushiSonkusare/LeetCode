3095ef844e9706a42e17edc61571c6b329042992
KhushiSonkusare <you@example.com>
2024-05-17 07:31:07 +0530

    Add Add Two Numbers

--- Add_Two_Numbers.cpp
+++ Add_Two_Numbers.cpp
@@ -1,19 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(); 
-        ListNode* current = dummy; 
-        int carry = 0;
-
-        while (l1 || l2 || carry) {
-            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
-            carry = sum / 10; 
-            current->next = new ListNode(sum % 10);
-            current = current->next;
-            if (l1) l1 = l1->next;
-            if (l2) l2 = l2->next;
+        std::string s1, s2;
+        while (l1 != nullptr) {
+            s1.append(std::to_string(l1->val));
+            l1 = l1->next;
         }
-
-        return dummy->next; 
+        while (l2 != nullptr) {
+            s2.append(std::to_string(l2->val));
+            l2 = l2->next;
+        }
+        std::reverse(s1.begin(), s1.end());
+        std::reverse(s2.begin(), s2.end());
+        int num1 = std::stoi(s1);
+        int num2 = std::stoi(s2);
+        int sum = num1 + num2;
+        std::string str = std::to_string(sum);
+        std::reverse(str.begin(), str.end());
+        ListNode* newNode = new ListNode(std::stoi(str.substr(0, 1)));
+        ListNode* temp = nullptr;
+        ListNode* curr = newNode;
+        for (int i = 1; i < str.size(); i++) {
+            temp = new ListNode(std::stoi(str.substr(i, 1)));
+            temp->next = nullptr;
+            curr->next = temp;
+            curr = temp;
+        }
+        return newNode;
     }
 };
