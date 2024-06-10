e6d76f7f8a4ae50446b6de4a2d709c1a489e2a1e
KhushiSonkusare <you@example.com>
2024-06-10 14:00:45 +0530

    Add Climbing Stairs

--- Climbing_Stairs.cpp
+++ Climbing_Stairs.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int first = 1, second = 2;
-        if(n == 0 || n == 1 || n ==2){
-            return n;
-        }else{
-            int next = 0;
-            for(int i = 0; i<n-2; i++){
-                next = first + second;
-                first = second;
-                second = next;
-            }
-            return next;
+        if (n == 0 || n == 1) {
+            return 1;
         }
+        return climbStairs(n-1) + climbStairs(n-2);
     }
 };
