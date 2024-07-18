2d308e8a6d88de115a306c9275500291d8db769c
KhushiSonkusare <you@example.com>
2024-07-18 06:14:50 +0530

    Add Unique Paths

--- Unique_Paths.cpp
+++ Unique_Paths.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    void paths(int i, int j, int m, int n, int &count) {
-        if (i == m - 1 && j == n - 1) {
-            count++;
-            return;
+    void paths(int i , int j , int m, int n, vector<int> &list){
+        if(i == m-1 && j == n-1){
+            list.push_back(1);
         }
 
-        if (i < m) {
-            paths(i + 1, j, m, n, count);
+        if(i<m){
+            paths(i+1,j,m,n,list);
         }
-        if (j < n) {
-            paths(i, j + 1, m, n, count);
-        }
+        if(j<n)
+            paths(i,j+1,m,n,list);
     }
 
     int uniquePaths(int m, int n) {
-        int count = 0;
-        paths(0, 0, m, n, count);
+        vector<int> list;
+        paths(0,0,m,n,list);
 
-        return count;
+        return list.size();
     }
 };
