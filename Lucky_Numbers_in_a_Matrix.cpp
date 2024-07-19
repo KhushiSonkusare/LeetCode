0b461a3fa2396cf12322d52b48f00c0aed508b00
KhushiSonkusare <you@example.com>
2024-07-19 06:32:16 +0530

    Add Lucky Numbers in a Matrix

--- Lucky_Numbers_in_a_Matrix.cpp
+++ Lucky_Numbers_in_a_Matrix.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>& matrix, int row, int col, int p) {
-        for (int i = 0; i < matrix.size(); i++) {
-            if (matrix[i][col] > p) {
-                return false;
-            }
-        }
-        return true;
-    }
     vector<int> luckyNumbers(vector<vector<int>>& matrix) {
-        vector<int> answer;
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int N = matrix.size(), M = matrix[0].size();
 
-        for (int i = 0; i < n; i++) {
-            int p = INT_MAX;
-            int t;
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] < p) {
-                    t = j;
-                    p = matrix[i][j];
+        vector<int> rowMin;
+        for (int i = 0; i < N; i++) {
+
+            int rMin = INT_MAX;
+            for (int j = 0; j < M; j++) {
+                rMin = min(rMin, matrix[i][j]);
+            }
+            rowMin.push_back(rMin);
+        }
+
+        vector<int> colMax;
+        for (int i = 0; i < M; i++) {
+
+            int cMax = INT_MIN;
+            for (int j = 0; j < N; j++) {
+                cMax = max(cMax, matrix[j][i]);
+            }
+            colMax.push_back(cMax);
+        }
+
+        vector<int> luckyNumbers;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (matrix[i][j] == rowMin[i] && matrix[i][j] == colMax[j]) {
+                    luckyNumbers.push_back(matrix[i][j]);
                 }
             }
-            if (check(matrix, i, t, p) == true) {
-                answer.push_back(p);
-            }
         }
-        return answer;
+
+        return luckyNumbers;
     }
 };
