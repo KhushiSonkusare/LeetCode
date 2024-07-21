17066ddf6451850c70a61b79b20302ac4e32da01
KhushiSonkusare <you@example.com>
2024-07-22 03:41:14 +0530

    Add The Employee That Worked on the Longest Task

--- The_Employee_That_Worked_on_the_Longest_Task.cpp
+++ The_Employee_That_Worked_on_the_Longest_Task.cpp
@@ -1,19 +1,15 @@
-class Solution 
-{
+class Solution {
 public:
-    int hardestWorker(int n, vector<vector<int>>& logs) 
-    {
-        int id=INT_MAX, maxi=INT_MIN, last=0;
-        
-        for(int i=0; i<logs.size(); i++)
-        {
-            if((logs[i][1] - last) > maxi || ((logs[i][1] - last)==maxi && id > logs[i][0]))
-            {
-                maxi = (logs[i][1] - last);
-                id = logs[i][0];
-            } 
-            last = logs[i][1];
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        int max = logs[0][1];
+        int maxi = logs[0][0];
+        for (int i = 1; i < logs.size(); i++) {
+            if (max < (logs[i][1] - logs[i - 1][1])) {
+                max = logs[i][1] - logs[i - 1][1];
+                maxi = logs[i][0];
+            }
         }
-        return id;
+
+        return maxi;
     }
 };
