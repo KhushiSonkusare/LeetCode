1dbdcd370dfa13f720d7f555a0b6cc1f1d8f4213
KhushiSonkusare <you@example.com>
2024-07-21 16:09:20 +0530

    Add Check if Matrix Is X-Matrix

--- Check_if_Matrix_Is_X-Matrix.cpp
+++ Check_if_Matrix_Is_X-Matrix.cpp
@@ -1,22 +1,18 @@
-bool checkXMatrix(vector<vector<int>>& grid) {
-        int p=0,q=grid[0].size()-1;
-        
-        for(int i=0;i<grid.size();++i){
-            for(int j=0;j<grid[0].size();++j){
-                if(i==j||(i==p&&j==q)){
-                    //diagonal elements
-                    if(grid[i][j]==0)
-                        return false;
-                }
-                else{
-                    //non diagonal elements
-                    if(grid[i][j]!=0)
-                        return false;
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        bool flag = true;
+        for(int i = 0; i<grid.size(); i++){
+            for(int j = 0; j<grid[i].size(); j++){
+                if(i == j){
+                    if(grid[i][j] == 0){
+                        flag = false;
+                        break;
+                    }
                 }
             }
-            p++;
-            q--;
         }
-        
-        return true;
+
+        return flag;
     }
+};
