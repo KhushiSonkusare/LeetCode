504ee75d8621aea647d196cba5339f1085e55b49
KhushiSonkusare <you@example.com>
2024-07-22 03:40:25 +0530

    Add The Employee That Worked on the Longest Task

--- The_Employee_That_Worked_on_the_Longest_Task.cpp
+++ The_Employee_That_Worked_on_the_Longest_Task.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hardestWorker(int n, vector<vector<int>>& logs) {
         int max = logs[0][1];
-        int maxi = logs[0][0];
+        int maxi = 0;
         for (int i = 1; i < logs.size(); i++) {
             if (max < (logs[i][1] - logs[i - 1][1])) {
                 max = logs[i][1] - logs[i - 1][1];
