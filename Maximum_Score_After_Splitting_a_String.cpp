d3593327f6c11047a7307a98fb65ad9aa01309aa
KhushiSonkusare <you@example.com>
2025-01-01 13:43:45 +0530

    Add Maximum Score After Splitting a String

---
+++ Maximum_Score_After_Splitting_a_String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int zeros = 0, ones = 0;
+        int maxScore = 0;
+
+        for (char c : s) {
+            if (c == '1') ones++;
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s[i] == '0') {
+                zeros++;
+            } else {
+                ones--; 
+            }
+
+            int score = zeros + ones;
+
+            if (score > maxScore) {
+                maxScore = score;
+            }
+        }
+
+        return maxScore;
+        
+    }
+};
