31c09b5de92a9f04226961b430481a62f13936d6
KhushiSonkusare <you@example.com>
2025-01-03 14:36:08 +0530

    Add Number of Ways to Split Array

--- Number_of_Ways_to_Split_Array.cpp
+++ Number_of_Ways_to_Split_Array.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
-        vector<long long> prefixSum(n); 
+        vector<int> prefixSum(n);
 
         prefixSum[0] = nums[0];
         for (int i = 1; i < n; i++) {
             prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
 
-        long long totalSum = prefixSum[n - 1];
+        int totalSum = prefixSum[n - 1];
         int count = 0;
         for (int i = 0; i < n - 1; i++) {
-            long long first = prefixSum[i];
-            long long last = totalSum - first;
+            int first = prefixSum[i];
+            int last = totalSum - first;
 
             if (first >= last) {
                 count++;
