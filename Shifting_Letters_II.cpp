eaadd87af3cf3895a15dec27dafdeb0aa5a1dc31
KhushiSonkusare <you@example.com>
2025-01-05 16:17:36 +0530

    Add Shifting Letters II

--- Shifting_Letters_II.cpp
+++ Shifting_Letters_II.cpp
@@ -3,7 +3,6 @@ public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         vector<long long> prefixSum(s.size(), 0);
 
-        // Calculate prefix sum of shifts
         for (int i = 0; i < shifts.size(); i++) {
             int add = -1;
             if (shifts[i][2] == 1) {
@@ -15,9 +14,7 @@ public:
         }
 
         for (int i = 0; i < s.size(); i++) {
-            int d = s[i] - 'a' + prefixSum[i];
-            d = (d % 26 + 26) % 26; 
-            s[i] = 'a' + d;
+            s[i] = (s[i] - 'a' + prefixSum[i] + 26) % 26 + 'a';  
         }
 
         return s;
