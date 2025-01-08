d041dda7d4b1c7e47112da84ce8a3ae6eb10df99
KhushiSonkusare <you@example.com>
2025-01-08 13:18:53 +0530

    Add Count Prefix and Suffix Pairs I

---
+++ Count_Prefix_and_Suffix_Pairs_I.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int n=words.size();
+        int ans=0;
+        for(int i=0; i<n-1; i++)
+            for(int j=i+1; j<n; j++)
+                ans+=(words[j].starts_with(words[i]) && words[j].ends_with(words[i]));
+        return ans;
+    }
+};
