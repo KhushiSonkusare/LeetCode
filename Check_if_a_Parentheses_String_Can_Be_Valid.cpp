9f2778950bcf221748ba02e4d3228dfb670367d7
KhushiSonkusare <you@example.com>
2025-01-12 18:50:48 +0530

    Add Check if a Parentheses String Can Be Valid

--- Check_if_a_Parentheses_String_Can_Be_Valid.cpp
+++ Check_if_a_Parentheses_String_Can_Be_Valid.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        if (s.size() % 2 == 1) {
-            return false;
-        }
-
-        int open = 0, unlocked = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            if (locked[i] == '0') {
-                unlocked++;
-            } else if (s[i] == '(') {
-                open++;
-            } else {
-                if (open > 0) {
-                    open--;
-                } else if (unlocked > 0) {
-                    unlocked--;
-                } else {
-                    return false;
-                }
-            }
-        }
-
-        int close = 0;
-        unlocked = 0;
-        for (int i = s.size() - 1; i >= 0; --i) {
-            if (locked[i] == '0') {
-                unlocked++;
-            } else if (s[i] == ')') {
-                close++;
-            } else {
-                if (close > 0) {
-                    close--;
-                } else if (unlocked > 0) {
-                    unlocked--;
-                } else {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+    stack<char> lock;
+    if (s.size() % 2 == 1) {
+        return false;
     }
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ')') {
+            if (lock.size() == 0) {
+                if (locked[i] == '1') {
+                    return false;
+                } else {
+                    lock.push('(');
+                }
+            } else {
+                lock.pop();
+            }
+        } else {
+            if (lock.size() != 0) {
+                if (locked[i] == '0') {
+                    lock.pop();
+                } else {
+                    lock.push('(');
+                }
+            } else {
+                lock.push('(');
+            }
+        }
+    }
+
+    return lock.size() == 0;
+}
+
+    
 };
