4274443fc325b04cf2a750e14c32029d103c0edc
KhushiSonkusare <you@example.com>
2025-01-12 18:38:35 +0530

    Add Check if a Parentheses String Can Be Valid

--- Check_if_a_Parentheses_String_Can_Be_Valid.cpp
+++ Check_if_a_Parentheses_String_Can_Be_Valid.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-    stack<char> lock;
-    if (s.size() % 2 == 1) {
+        stack<char> lock;
+        if(s.size()%2 == 1)
+            return false;
+        else{
+            for(int i = 0; i<s.size(); i++){
+                if(s[i] == ')'){
+                    if(lock.size() == 0){
+                        if(locked[i] == 1)
+                            return false;
+                        else{
+                            s[i] = '(';
+                            lock.push('(');
+                        }
+                    }
+                    else{
+                        if(lock.size()!=0)
+                            lock.pop();
+                        else
+                            return false;
+                    }
+                }
+                else{
+                    if(lock.size()!=0){
+                        if(locked[i] == 0){
+                            lock.pop();
+                        }else{
+                            lock.push('(');
+                        }
+                    }else{
+                        lock.push('(');
+                    }
+                }
+            }
+
+            if(lock.size() == 0){
+                return true;
+            }else
+                return false;
+        }
         return false;
     }
-
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == ')') {
-            if (lock.size() == 0) {
-                if (locked[i] == '1') {
-                    return false;
-                } else {
-                    lock.push('(');
-                }
-            } else {
-                lock.pop();
-            }
-        } else {
-            if (lock.size() != 0) {
-                if (locked[i] == '0') {
-                    lock.pop();
-                } else {
-                    lock.push('(');
-                }
-            } else {
-                lock.push('(');
-            }
-        }
-    }
-
-    return lock.size() == 0;
-}
-
-    
 };
