b0726cd5f7ab27f4a1d069b398ee6bafb2f6e081
KhushiSonkusare <you@example.com>
2025-01-28 19:27:30 +0530

    Add Valid Palindrome

--- Valid_Palindrome.cpp
+++ Valid_Palindrome.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool check(const string& s, int left, int right) {
-        if (left >= right) 
-            return true; // Base case: Pointers have crossed or are at the same position
+    bool check(string s, long long left, long long right){
+        
+         if (left >= right) 
+            return true;
 
-        if (s[left] != s[right]) 
-            return false; // Mismatch found
-
-        return check(s, left + 1, right - 1); // Recursive call for the next pair
+        if (s[left] == s[right]) 
+            return check(s, left + 1, right - 1); 
+        
+        return false;
     }
-
     bool isPalindrome(string s) {
-        // Step 1: Preprocess the string (lowercase and remove non-alphanumeric characters)
         string modifiedString = "";
+
         for (char c : s) {
             if (isalnum(c)) {
                 modifiedString += tolower(c);
             }
         }
+        return check(modifiedString, 0, modifiedString.size()-1);
 
-        // Step 2: Use recursion to check if the string is a palindrome
-        return check(modifiedString, 0, modifiedString.size() - 1);
     }
 };
