52d20eee4b3be0ff6b82457d5d6b30784f99598d
KhushiSonkusare <you@example.com>
2025-01-28 19:56:30 +0530

    Add Pow(x, n)

--- Pow(x,_n).cpp
+++ Pow(x,_n).cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    double power(double x, long long n) {
+    double power(double x, int n) {
         if (n == 0) return 1.0;
-        
+        if(x == 1) return 1.0;
         if (n < 0) {
             x = 1 / x;
             n = -n;
         }
-
+        double half = power(x, n / 2);
         if (n % 2 == 0) {
-            double half = power(x, n / 2);
             return half * half;
         } else {
-            return x * power(x, n - 1);
+            return x * half * half;
         }
     }
 
