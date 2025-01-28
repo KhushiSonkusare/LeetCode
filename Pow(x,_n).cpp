bc9062299322349c92835084ca80d771817a5df7
KhushiSonkusare <you@example.com>
2025-01-28 19:45:48 +0530

    Add Pow(x, n)

--- Pow(x,_n).cpp
+++ Pow(x,_n).cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     double power(double x, int n) {
-        if (n == 0) return 1.0;
-        if (n < 0) {
-            x = 1 / x;
-            n = -n;
+        if (n == 0) {
+            return 1;
         }
-        double half = power(x, n / 2);
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return x * half * half;
-        }
+
+        return x * power(x, n - 1);  
     }
 
     double myPow(double x, int n) {
-        return power(x, n);
+        if (n > 0) {
+            return power(x, n);
+        } else if (n == 0) {
+            return 1;  
+        } else if (x == 0) {
+            return 0;  
+        } else {
+            return 1 / power(x, -n);  
+        }
     }
 };
