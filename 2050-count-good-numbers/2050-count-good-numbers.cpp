fa8b14460eaebee65eb0e9c90b444d235a40d205
Khushi Sonkusare <91463156+KhushiSonkusare@users.noreply.github.com>
2025-06-06 00:15:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (37.1%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,21 +1,27 @@
 class Solution {
-    static const long long MOD = 1'000'000'007;
-
 public:
-    long long myPow(long long x, long long n, long long mod) {
-        long long ans = 1;
-        x %= mod;
-        while (n > 0) {
-            if (n % 2 != 0) {
-                ans = (ans * x) % mod;
-            }
-            x = (x * x) % mod;
-            n = n / 2;
+    const int MOD = 1e9 + 7;
+
+    long long expo(long long a, long long b){
+        long long result = 1;
+        a %= MOD;
+
+        while (b > 0) {
+            if (b % 2 == 1)
+                result = (result * a) % MOD;
+
+            a = (a * a) % MOD;
+            b /= 2;
         }
-        return ans;
+
+        return result;
     }
 
     int countGoodNumbers(long long n) {
-        return (int)((myPow(5, n / 2 + n % 2, MOD) * myPow(4, n / 2, MOD)) % MOD);
+        long long evenPositions = (n + 1) / 2;
+        long long oddPositions = n / 2;
+
+        long long ans = (expo(5, evenPositions) * expo(4, oddPositions)) % MOD;
+        return (int)ans;
     }
 };
