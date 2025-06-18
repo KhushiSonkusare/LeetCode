8991dc0dd49db20f5f48506962bbe3b042aa4e7a
Khushi Sonkusare <91463156+KhushiSonkusare@users.noreply.github.com>
2025-06-18 12:35:49 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int N = n + m - 2;
-            int r = m - 1; 
-            double res = 1;
-            
-            for (int i = 1; i <= r; i++)
-                res = res * (N - r + i) / i;
-            return (int)res;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int up = 0, left = 0;  // ✅ Initialize to 0
+                if (i > 0) {
+                    up = dp[i - 1][j];
+                }
+                if (j > 0) {
+                    left = dp[i][j - 1];
+                }
+
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
