85761d2704a232affa33bbc4040ed96ddf3cab16
Khushi Sonkusare <91463156+KhushiSonkusare@users.noreply.github.com>
2025-06-30 23:38:36 +0530

    Time: 1 ms (53.04%), Space: 20 MB (53.49%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &isConnected, int node, vector<int> &visited) {
-        stack<int> st;
-        st.push(node);
+    int findParent(int x, vector<int> &parent) {
+        if (parent[x] != x)
+            parent[x] = findParent(parent[x], parent); 
+        return parent[x];
+    }
 
-        while (!st.empty()) {
-            int curr = st.top();
-            st.pop();
-            if (!visited[curr]) {
-                visited[curr] = 1;
-                for (int j = 0; j < isConnected.size(); j++) {
-                    if (isConnected[curr][j] == 1 && !visited[j]) {
-                        st.push(j);
-                    }
-                }
-            }
+    void unionSet(int v, int u, vector<int> &parent) {
+        int parentV = findParent(v, parent);
+        int parentU = findParent(u, parent);
+        
+        if (parentV != parentU) {
+            parent[parentV] = parentU;
         }
     }
 
-    int findCircleNum(vector<vector<int>> &isConnected) {
-        int n = isConnected.size();
-        vector<int> visited(n, 0);
-        int count = 0;
+    vector<vector<int>> getComponents(vector<vector<int>> &matrix) {
+        int V = matrix.size();
+        vector<int> parent(V);
+        for (int i = 0; i < V; i++) parent[i] = i;
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(isConnected, i, visited);
-                count++;
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (matrix[i][j] == 1 && i != j) {
+                    unionSet(i, j, parent);
+                }
             }
         }
 
-        return count;
+        for (int i = 0; i < V; i++) {
+            parent[i] = findParent(i, parent);
+        }
+
+        unordered_map<int, vector<int>> resMap;
+        for (int i = 0; i < V; i++) {
+            resMap[parent[i]].push_back(i);
+        }
+
+        vector<vector<int>> res;
+        for (auto &entry : resMap) {
+            res.push_back(entry.second);
+        }
+
+        return res;
+    }
+
+    int findCircleNum(vector<vector<int>> &isConnected) {
+        return getComponents(isConnected).size();
     }
 };
