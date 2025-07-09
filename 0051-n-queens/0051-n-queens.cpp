eca6fb7549ebaf571f167ef59c38d3f16cf8f178
Khushi Sonkusare <91463156+KhushiSonkusare@users.noreply.github.com>
2025-07-09 06:20:41 +0530

    Time: 3 ms (65.54%), Space: 10.1 MB (92.74%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     bool isSafe(int row, int col, vector<string>& board, int n) {
-
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q') return false;
+        }
 
-        for (int j = col; j >= 0; j--)
-            if (board[row][j] == 'Q') return false;
-
-        for (int i = row, j = col; i < n && j >= 0; i++, j--)
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
             if (board[i][j] == 'Q') return false;
+        }
 
         return true;
     }
 
-    void helper(vector<string>& board, vector<vector<string>>& ans, int col, int n) {
-        if (col == n) {
+    void helper(vector<string>& board, vector<vector<string>>& ans, int row, int n) {
+        if (row == n) {
             ans.push_back(board);
             return;
         }
 
-        for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
             if (isSafe(row, col, board, n)) {
                 board[row][col] = 'Q';
-                helper(board, ans, col + 1, n);
-                board[row][col] = '.'; 
+                helper(board, ans, row + 1, n);
+                board[row][col] = '.';
             }
         }
     }
