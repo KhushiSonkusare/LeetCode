bc1736a3ba87c9245e1fa981e611c8d513a08aa4
Khushi Sonkusare <91463156+KhushiSonkusare@users.noreply.github.com>
2025-09-04 14:10:38 +0530

    Time: 29 ms (92.41%), Space: 53.4 MB (85.68%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool isPalindrome(const string &s, int left, int right) {
-        while (left < right) {
-            if (s[left++] != s[right--]) return false;
+    bool checkPalindrome(string &s, int left, int right){
+        while(left <= right){
+            if(s[left++] != s[right--]) return false;
         }
         return true;
     }
 
-    void helper(vector<vector<string>> &ans, vector<string> &curr, int ind, const string &s) {
-        if (ind == s.size()) {
+    void helper(vector<vector<string>> &ans, vector<string> &curr, int ind, string &s){
+        if(ind == s.size()){
             ans.push_back(curr);
             return;
         }
 
-        for (int i = ind; i < s.size(); i++) {
-            if (isPalindrome(s, ind, i)) {
-                curr.push_back(s.substr(ind, i - ind + 1)); 
-                helper(ans, curr, i + 1, s);
-                curr.pop_back();
+        for(int i = ind; i < s.size(); i++){
+            if(checkPalindrome(s, ind, i)){
+                curr.push_back(s.substr(ind, i - ind + 1));
+                helper(ans, curr, i + 1, s);               
+                curr.pop_back();                        
             }
         }
     }
