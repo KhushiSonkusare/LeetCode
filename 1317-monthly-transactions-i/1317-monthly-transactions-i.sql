abc741801602013b286ea3b256275bb85c9df68e
Khushi Sonkusare <91463156+KhushiSonkusare@users.noreply.github.com>
2025-09-28 14:56:27 +0530

    Time: 542 ms (84.92%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,9 +1,10 @@
-SELECT 
-    LEFT(trans_date, 7) AS month,
+select
+    DATE_FORMAT(trans_date, '%Y-%m') as month,
     country,
-    COUNT(*) AS trans_count,
-    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
-    SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
-FROM Transactions
-GROUP BY month, country;
+    count(*) as trans_count,
+    sum(if(state = 'approved', 1, 0)) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from 
+    Transactions
+group by DATE_FORMAT(trans_date, '%Y-%m'), country;
